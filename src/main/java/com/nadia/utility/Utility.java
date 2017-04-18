be667ed7ebf1905fb6b9ef7f26f4031e6b4b6797
javadev <javadev75@gmail.com>
2017-04-18 08:55:22 +0300

    Add fetch file method.

--- src/main/java/com/nadia/utility/Utility.java
+++ src/main/java/com/nadia/utility/Utility.java
@@ -1,11 +1,140 @@
 package com.nadia.utility;
 
+import java.util.*;
+
 public class Utility {
+    private static final Map<String, List<String>> DEFAULT_HEADER_FIELDS = new HashMap<String, List<String>>() { {
+        put("Content-Type", Arrays.asList("application/json", "charset=utf-8"));
+    } };
+    private static final Set<String> SUPPORTED_HTTP_METHODS = new HashSet<String>(
+        Arrays.asList("GET", "POST", "PUT", "DELETE"));
+    private static final int BUFFER_LENGTH_1024 = 1024;
+    private static final int RESPONSE_CODE_400 = 400;
+
     public static interface Callback {
         void report(String data);
     }
 
-    public static void downloadFiles(String threads, String outputFolder, String linksFileName) {
+    public static class FetchResponse {
+        private final boolean ok;
+        private final int status;
+        private final Map<String, List<String>> headerFields;
+        private final java.io.ByteArrayOutputStream stream;
+
+        public FetchResponse(final boolean ok, final int status, final Map<String, List<String>> headerFields,
+            final java.io.ByteArrayOutputStream stream) {
+            this.ok = ok;
+            this.status = status;
+            this.stream = stream;
+            this.headerFields = headerFields;
+        }
+
+        public boolean isOk() {
+            return ok;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public Map<String, List<String>> getHeaderFields() {
+            return headerFields;
+        }
+
+        public byte[] blob() {
+            return stream.toByteArray();
+        }
+
+        public String text() {
+            try {
+                return stream.toString("UTF-8");
+            } catch (java.io.UnsupportedEncodingException ex) {
+                throw new UnsupportedOperationException(ex);
+            }
+        }
+    }
+
+    static class NoHostnameVerifier implements javax.net.ssl.HostnameVerifier {
+        public boolean verify(String hostname, javax.net.ssl.SSLSession session) {
+              return true;
+        }
+    }
+
+    public static FetchResponse fetch(final String url) {
+        return fetch(url, null, null, DEFAULT_HEADER_FIELDS, null, null);
+    }
+
+    public static FetchResponse fetch(final String url, final String method, final String body,
+        final Map<String, List<String>> headerFields, final Integer connectTimeout, final Integer readTimeout) {
+        final String localMethod;
+        if (SUPPORTED_HTTP_METHODS.contains(method)) {
+            localMethod = method;
+        } else {
+            localMethod = "GET";
+        }
+        try {
+            final java.net.URL localUrl = new java.net.URL(url);
+            final java.net.HttpURLConnection connection = (java.net.HttpURLConnection) localUrl.openConnection();
+            connection.setRequestMethod(localMethod);
+            if (connectTimeout != null) {
+                connection.setConnectTimeout(connectTimeout);
+            }
+            if (readTimeout != null) {
+                connection.setReadTimeout(readTimeout);
+            }
+            if (connection instanceof javax.net.ssl.HttpsURLConnection) {
+                ((javax.net.ssl.HttpsURLConnection) connection).setHostnameVerifier(new NoHostnameVerifier());
+            }
+            if (headerFields != null) {
+                for (final Map.Entry<String, List<String>> header : headerFields.entrySet()) {
+                    connection.setRequestProperty(header.getKey(), join(header.getValue(), ";"));
+                }
+            }
+            if (body != null) {
+                connection.setDoOutput(true);
+                final java.io.DataOutputStream outputStream =
+                    new java.io.DataOutputStream(connection.getOutputStream());
+                outputStream.writeBytes(body);
+                outputStream.flush();
+                outputStream.close();
+            }
+            final int responseCode = connection.getResponseCode();
+            final java.io.InputStream inputStream;
+            if (responseCode < RESPONSE_CODE_400) {
+                inputStream = connection.getInputStream();
+            } else {
+                inputStream = connection.getErrorStream();
+            }
+            final java.io.ByteArrayOutputStream result = new java.io.ByteArrayOutputStream();
+            final byte[] buffer = new byte[BUFFER_LENGTH_1024];
+            int length;
+            while ((length = inputStream.read(buffer)) != -1) {
+                result.write(buffer, 0, length);
+            }
+            inputStream.close();
+            return new FetchResponse(responseCode < RESPONSE_CODE_400, responseCode, connection.getHeaderFields(),
+                result);
+        } catch (java.io.IOException ex) {
+            throw new UnsupportedOperationException(ex);
+        }
+    }
+
+    static <T> String join(final Iterable<T> iterable, final String separator) {
+        final StringBuilder sb = new StringBuilder();
+        int index = 0;
+        for (final T item : iterable) {
+            if (index > 0) {
+                sb.append(separator);
+            }
+            sb.append(item.toString());
+            index += 1;
+        }
+        return sb.toString();
+    }
+
+    public static void downloadFiles(String threads, String outputFolder, String linksFileName, Callback callback) {
+        String text = fetch("https://www.linkedin.com/jobs/view/287941696/").text();
+        System.out.println("text - " + text);
     }
 
     public static void main(String[] args) {
@@ -22,7 +151,11 @@ public class Utility {
             } else if (args.length >= 3) {
                 linksFileName = args[2];
             }
-            downloadFiles(threads, outputFolder, linksFileName);
+            Callback callback = new Callback() {
+                public void report(String data) {
+                }
+            };
+            downloadFiles(threads, outputFolder, linksFileName, callback);
         }
     }
 }
