4fb9b9f174616dd1a8971c452aef3889be4d5b9d
javadev <javadev75@gmail.com>
2017-04-18 08:38:59 +0300

    Implement main method in Utility class.

--- src/main/java/com/nadia/utility/Utility.java
+++ src/main/java/com/nadia/utility/Utility.java
@@ -1,5 +1,28 @@
 package com.nadia.utility;
 
 public class Utility {
+    public static interface Callback {
+        void report(String data);
+    }
 
+    public static void downloadFiles(String threads, String outputFolder, String linksFileName) {
+    }
+
+    public static void main(String[] args) {
+        String threads = "5";
+        String outputFolder = "output_folder";
+        String linksFileName = "links.txt";
+        if (args.length == 0) {
+            System.out.println("Usage: java -jar utility.jar 5 output_folder links.txt");
+        } else {
+            if (args.length >= 1) {
+                threads = args[0];
+            } else if (args.length >= 2) {
+                outputFolder = args[1];
+            } else if (args.length >= 3) {
+                linksFileName = args[2];
+            }
+            downloadFiles(threads, outputFolder, linksFileName);
+        }
+    }
 }
